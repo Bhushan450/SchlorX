470e30469869f6635df968a4ec46ef5ca0fb186b
bhushan450 <bhushanmhaske45@gmail.com>
2026-08-02 02:23:47 +0530

    Fixes typo

--- backend/modules/auth/dto/register.dto.js
+++ backend/modules/auth/dto/register.dto.js
@@ -4,11 +4,11 @@ import BaseDto from "../../../common/dto/base.dto.js"
 class RegisterDto extends BaseDto {
 
     static schema = joi.object({
-        name: Joi.string().trim().min(2).max(30).required(),
-        email: Joi.string().trim().email().required(),
-        phone: Joi.string().pattern(/^\d{10}$/).required(),
-        password: Joi.string().min(6).required(),
-        role: Joi.string().valid("admin", "teacher").required(),
+        name: joi.string().trim().min(2).max(30).required(),
+        email: joi.string().trim().email().required(),
+        phone: joi.string().pattern(/^\d{10}$/).required(),
+        password: joi.string().min(6).required(),
+        role: joi.string().valid("admin", "teacher").required(),
         classAssigned: joi.string().optional(),
     })  
 };
