f0cd09c31e16103b9772c73d4ce9bc5ad2a3d287
bhushan450 <bhushanmhaske45@gmail.com>
2026-08-02 01:15:50 +0530

    Add JWT utility functions for token generation and verification

---
+++ backend/common/utils/jwt.js
@@ -0,0 +1,52 @@
+import crypto from "crypto" // jsonwebToken
+import { raw } from "express";
+import jwt from "jsonwebtoken"
+
+// genearte Access Token
+const generateAccessToken = (payload)=>{
+   
+   return jwt.sign(payload , process.env.JWT_ACCESS_SECRET , {
+      expiresIn: process.env.JWT_ACCESS_EXPIRES || '15m'
+   });
+};
+
+// Verify Access Token 
+const verifyAccessToken = (token)=>{
+   return jwt.verify(token,process.env.JWT_ACCESS_SECRET);
+};
+
+//generate Refresh Token
+const generateRefreshToken = (payload)=>{
+   jwt.sign(payload, process.env.JWT_REFRESH_SECRET , {
+      expiresIn: process.env.JWT_REFRESH_EXPIRES||'7d'
+   });
+};
+
+// verify Refresh Token 
+const verifyRefreshToken = (token)=>{
+   return jwt.verify(token , process.env.JWT_REFRESH_SECRET)
+};
+
+const generateResetTokens = ()=>{
+   const rawToken = crypto.randomBytes(32).toString("hex");
+
+   const hashedToken = crypto
+   .createHash("sha256")
+   .update(rawToken)
+   .digest("hex")
+
+   return {rawToken , hashedToken}
+}
+
+// verify resetToken
+// const verifyResetToken = (token)=>{
+//    return jwt.verify(token,process.env.JWT_RESET_EXPIRES)
+// }
+
+export {
+    generateAccessToken,
+    verifyAccessToken,
+    generateRefreshToken,
+    verifyRefreshToken,
+    generateResetTokens
+}
