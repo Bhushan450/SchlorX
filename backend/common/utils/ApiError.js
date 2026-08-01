3f97a91671552db1689641f3118e189678ab188c
bhushan450 <bhushanmhaske45@gmail.com>
2026-08-02 01:14:35 +0530

    Add ApiError class for standardized error handling

---
+++ backend/common/utils/ApiError.js
@@ -0,0 +1,31 @@
+class ApiError extends Error{
+
+    constructor(statuscode, message){
+        super(message)
+        this.statuscode=statuscode
+        this.isOperational = true // this property commonly used to distinguish bwtn operational errors / programmming errors
+        Error.captureStackTrace(this,this.constructor)
+    }
+
+    static badRequest(message="Bad Request"){
+        return new ApiError(400,message);
+    };
+
+    static unauthorised(message="Unauthorised Request"){
+        return new ApiError(401,message);
+    };
+
+    static notFound(message="User not found"){
+        return new ApiError(412,message);
+    };
+
+    static conflict(message="Conflict!"){
+        return new ApiError(409,message);
+    };
+
+    static forbidden(message="Forbidden"){
+        return new ApiError(412,message);
+    };
+}
+
+export default ApiError;
