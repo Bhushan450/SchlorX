4b6e54ecac38222f3e19a4f4acf3fbc3a2ca88d2
bhushan450 <bhushanmhaske45@gmail.com>
2026-08-02 02:20:51 +0530

    Fix typo in ApiError import and correct error handling in validation middleware

--- backend/common/middleware/validate.js
+++ backend/common/middleware/validate.js
@@ -1,4 +1,4 @@
-import ApiError from "../utils/Api-error.js";
+import ApiError from "../utils/ApiError.js";
 // import BaseDto from "../dto/base.dto.js";
 
 
@@ -6,7 +6,7 @@ const validate = (Dtoclass)=>{  // checks if the req.body is as per the schema o
     return (req,res,next)=>{
         const{errors,value} = Dtoclass.validateData(req.body); // this 'validateData' is from baseDto 
 
-        if(errors) throw ApiError.badRequest(error.join("; "))
+        if(errors) throw ApiError.badRequest(errors.join("; "))
 
             req.body = value; // value-> contains value that are required only ,pass the only values that are required 
 
