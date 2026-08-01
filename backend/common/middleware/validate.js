8c4ae70b5a7c48398e4c52c3c1c62a057c3b3001
bhushan450 <bhushanmhaske45@gmail.com>
2026-08-02 01:13:34 +0530

    Add validation middleware to check request body against schema

---
+++ backend/common/middleware/validate.js
@@ -0,0 +1,17 @@
+import ApiError from "../utils/Api-error.js";
+// import BaseDto from "../dto/base.dto.js";
+
+
+const validate = (Dtoclass)=>{  // checks if the req.body is as per the schema or not 
+    return (req,res,next)=>{
+        const{errors,value} = Dtoclass.validateData(req.body); // this 'validateData' is from baseDto 
+
+        if(errors) throw ApiError.badRequest(error.join("; "))
+
+            req.body = value; // value-> contains value that are required only ,pass the only values that are required 
+
+            next();
+    }
+}
+
+export default validate 
