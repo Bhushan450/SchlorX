888bd8f301fc417a30f00698fe73ce609f146686
bhushan450 <bhushanmhaske45@gmail.com>
2026-08-02 23:10:50 +0530

    Refactor auth routes: streamline imports and remove unnecessary authorization for registration

--- backend/modules/auth/auth.routes.js
+++ backend/modules/auth/auth.routes.js
@@ -2,18 +2,18 @@ import * as authController from "./auth.controller.js"
 import { Router } from "express"
 import LoginDto from "./dto/login.dto.js"
 import RegisterDto from "./dto/register.dto.js"
-import forgotPasswordDto from "./dto/forgotPassword.dto.js"
-import resetPasswordToken from "./dto/forgotPassword.dto.js"
-import validateData from "../../common/middleware/validate.js"
+import ForgotPasswordDto from "./dto/forgotPassword.dto.js"
+import ResetPasswordDto from "./dto/forgotPassword.dto.js"
+import validate from "../../common/middleware/validate.js"
 import {authenticate, authorize} from "./auth.middleware.js"
 
 const router = Router();
 
-router.post('/register', authenticate, authorize("admin"),validate(RegisterDto), authController.register )
+router.post('/register', authenticate,validate(RegisterDto), authController.register )
 router.post('/login', validate(LoginDto) , authController.login )
 router.post('/logout', authenticate, authController.logout )
-router.post('/refresh-tokens/:token', authController.refresh )
-router.post('/forgot-password', validate(forgotPasswordDto) , authController.forgotPassword )
+router.post('/refresh-tokens', authController.refresh )
+router.post('/forgot-password', validate(ForgotPasswordDto) , authController.forgotPassword )
 router.post('/reset-password', validate(ResetPasswordDto) , authController.resetPassword )
 router.get('/getMe',authenticate , authController.getProfile )
 
