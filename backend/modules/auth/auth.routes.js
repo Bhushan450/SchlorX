6dc93c0e1aa9f8f498c816a98b4a5946c9122738
bhushan450 <bhushanmhaske45@gmail.com>
2026-08-02 23:53:29 +0530

    Fix import path for ResetPasswordDto and update reset-password route to include token parameter

--- backend/modules/auth/auth.routes.js
+++ backend/modules/auth/auth.routes.js
@@ -3,18 +3,20 @@ import { Router } from "express"
 import LoginDto from "./dto/login.dto.js"
 import RegisterDto from "./dto/register.dto.js"
 import ForgotPasswordDto from "./dto/forgotPassword.dto.js"
-import ResetPasswordDto from "./dto/forgotPassword.dto.js"
+import ResetPasswordDto from "./dto/resetPassword.dto.js"
 import validate from "../../common/middleware/validate.js"
 import {authenticate, authorize} from "./auth.middleware.js"
+import { valid } from "joi"
 
 const router = Router();
 
-router.post('/register', authenticate,validate(RegisterDto), authController.register )
+router.post('/register',validate(RegisterDto), authController.register )
 router.post('/login', validate(LoginDto) , authController.login )
 router.post('/logout', authenticate, authController.logout )
 router.post('/refresh-tokens', authController.refresh )
 router.post('/forgot-password', validate(ForgotPasswordDto) , authController.forgotPassword )
-router.post('/reset-password', validate(ResetPasswordDto) , authController.resetPassword )
+router.post('/reset-password/:token', validate(ResetPasswordDto) , authController.resetPassword )
+router.get('/verify-email/:token' , authController.verifyEmail)
 router.get('/getMe',authenticate , authController.getProfile )
 
 export default router;
