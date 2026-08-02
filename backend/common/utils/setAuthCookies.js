2eb3ac5c08631556e0144a62bf904a387c53cdab
bhushan450 <bhushanmhaske45@gmail.com>
2026-08-03 00:06:54 +0530

    Add utility function to set authentication cookies with access and refresh tokens

---
+++ backend/common/utils/setAuthCookies.js
@@ -0,0 +1,18 @@
+// set the cookies 
+
+const setAuthCookies = (res, accessToken, refreshToken) => {
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 15 * 60 * 1000,
+    });
+};
+
+export default setAuthCookies;
