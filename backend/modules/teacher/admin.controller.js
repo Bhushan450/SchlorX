d0fbac5866949efd99e07dc771bc411c3ca5d991
bhushan450 <bhushanmhaske45@gmail.com>
2026-08-02 23:37:41 +0530

    Remove commented-out code for createTeacher and assignClassToTeacher functions in admin controller

--- backend/modules/teacher/admin.controller.js
+++ backend/modules/teacher/admin.controller.js
@@ -1,13 +1,6 @@
 import * as adminService from "./admin.service.js"
 import ApiResponse from "../../common/utils/ApiResponse.js"
 
-
-// const createTeacher = async (req,res)=>{
-//     // register a user
-//     const teacher = await adminService.createTeacher(req.body);
-//     ApiResponse.created(res,"Teacher is created", teacher)
-// };
-
 const getTeacherById = async (req,res)=>{
     // get teacher with id
     const teacher = await adminService.getTeacherById(req.params.Id);
@@ -23,12 +16,6 @@ const deleteTeacher = async (req,res)=>{
     await adminService.deleteTeacher(req.params.id);
     ApiResponse.ok(res,"Teacher is deleted")
 };
-
-// const assignClassToTeacher = async (req,res)=>{
-//     // delete teacher from DB
-//     await authService.assignClassToTeacher(req.params.id , req.class.id);
-//     ApiResponse.created(res,"Class assigned successfully")
-// };
 
 export {
     createTeacher,
