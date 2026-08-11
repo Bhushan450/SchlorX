e93e2b062ddd4b38a3944674f366bc97a61a75ce
bhushan450 <bhushanmhaske45@gmail.com>
2026-08-11 09:22:01 +0530

    Refactor setAuthCookies to use environment-based cookie security settings

--- backend/common/utils/setAuthCookies.js
+++ backend/common/utils/setAuthCookies.js
@@ -1,16 +1,18 @@
 // set the cookies 
 
 const setAuthCookies = (res, accessToken, refreshToken) => {
+    const isProd = process.env.NODE_ENV === "production";
+
     res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
-        secure: true,
-        sameSite: "strict",
+        secure: isProd,
+        sameSite: isProd ? "strict" : "lax",
         maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.cookie("accessToken", accessToken, {
         httpOnly: true,
-        secure: true,
-        sameSite: "strict",
+        secure: isProd,
+        sameSite: isProd ? "strict" : "lax",
         maxAge: 15 * 60 * 1000,
     });
 };
