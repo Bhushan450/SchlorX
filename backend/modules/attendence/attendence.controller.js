761789080a72e62a28b96a9c1135bf02d068d09f
bhushan450 <bhushanmhaske45@gmail.com>
2026-08-12 19:23:10 +0530

    Add attendance controller with methods for marking and fetching attendance

---
+++ backend/modules/attendence/attendence.controller.js
@@ -0,0 +1,24 @@
+import * as attendenceService from "./attendence.service.js" 
+import ApiResponse from "../../common/utils/ApiResponse.js"
+
+const attendance = async (req,res)=>{
+
+    const attendenceRes = await attendenceService.attendence(req.body,req.user.classAssigned);
+    ApiResponse.created(res,"Attendence marked", attendenceRes);
+};
+const getAttendanceByDate = async (req,res)=>{
+
+    const attendenceRes = await attendenceService.getAttendanceByDate(req.body,req.user.classAssigned);
+    ApiResponse.created(res,"Attendence fetched", attendenceRes);
+};
+const getAttendanceByStudent = async (req,res)=>{
+
+    const attendenceRes = await attendenceService.getAttendanceByStudent(req.params.id,req.user.classAssigned);
+    ApiResponse.created(res,"Student Attendence fetched", attendenceRes);
+};
+
+export {
+    attendance,
+    getAttendanceByDate,
+    getAttendanceByStudent,
+}
