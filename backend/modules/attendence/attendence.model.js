d736dbbc36aa5efddd8dba44321cfb42685df40a
bhushan450 <bhushanmhaske45@gmail.com>
2026-08-12 19:23:24 +0530

    Add attendance model with schema and unique index for student attendance

---
+++ backend/modules/attendence/attendence.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const attendenceSchema = new mongoose.Schema(
+  {
+    student: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Student",
+      required: true,
+    },
+
+    class: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Class",
+      required: true,
+    },
+
+    date: {
+      type: Date,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["Present", "Absent"],
+      required: true,
+    },
+  },{ timestamps: true,});
+
+// Prevent duplicate attendance for the same student on the same date
+attendanceSchema.index(
+  { student: 1, date: 1 },
+  { unique: true }
+);
+
+export default mongoose.model("Attendence", attendenceSchema);
