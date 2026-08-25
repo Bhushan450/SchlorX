2e45749a2662c4e3c12b239205d93d28e24b24e9
bhushan450 <bhushanmhaske45@gmail.com>
2026-08-25 23:05:27 +0530

    Fix formatting and improve error handling in database connection function

--- backend/common/config/db.js
+++ backend/common/config/db.js
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
 
-const connectDb = async ()=>{
+const connectDb = async () => {
 
     try {
         const connect = await mongoose.connect(process.env.MONGODB_URI);
 
         console.log(`mongoDb connected!: ${connect.connection.host}`);
-        
+
     } catch (error) {
         console.log("Failed to connect mongoDB:", error);
-        
+        throw error;
+        process.exit(1);
     }
 }
 
