c1a7bba4753b6e2127af7fe19783d38ce0d9c4c9
bhushan450 <bhushanmhaske45@gmail.com>
2026-08-25 23:07:01 +0530

    Refactor attendance controller for improved readability and consistency

--- backend/modules/attendence/attendence.controller.js
+++ backend/modules/attendence/attendence.controller.js
@@ -1,20 +1,20 @@
-import * as attendenceService from "./attendence.service.js" 
+import * as attendenceService from "./attendence.service.js"
 import ApiResponse from "../../common/utils/ApiResponse.js"
 
-const attendance = async (req,res)=>{
+const attendance = async (req, res) => {
 
-    const attendenceRes = await attendenceService.attendence(req.body,req.user.classAssigned);
-    ApiResponse.created(res,"Attendence marked", attendenceRes);
+    const attendenceRes = await attendenceService.attendence(req.body, req.user.classAssigned);
+    ApiResponse.created(res, "Attendence marked", attendenceRes);
 };
-const getAttendanceByDate = async (req,res)=>{
+const getAttendanceByDate = async (req, res) => {
 
-    const attendenceRes = await attendenceService.getAttendanceByDate(req.body,req.user.classAssigned);
-    ApiResponse.created(res,"Attendence fetched", attendenceRes);
+    const attendenceRes = await attendenceService.getAttendanceByDate(req.body, req.user.classAssigned);
+    ApiResponse.ok(res, "Attendence fetched", attendenceRes);
 };
-const getAttendanceByStudent = async (req,res)=>{
+const getAttendanceByStudent = async (req, res) => {
 
-    const attendenceRes = await attendenceService.getAttendanceByStudent(req.params.id,req.user.classAssigned);
-    ApiResponse.created(res,"Student Attendence fetched", attendenceRes);
+    const attendenceRes = await attendenceService.getAttendanceByStudent(req.params.studentId, req.user.classAssigned);
+    ApiResponse.ok(res, "Student Attendence fetched", attendenceRes);
 };
 
 export {
