59930a8c3de83aa73641a712809796035878bcfb
bhushan450 <bhushanmhaske45@gmail.com>
2026-09-15 22:15:31 +0530

    Refactor student controller for improved code readability and consistency

--- backend/modules/student/student.controller.js
+++ backend/modules/student/student.controller.js
@@ -2,36 +2,36 @@ import * as studentService from "../student/student.service.js"
 import ApiResponse from "../../common/utils/ApiResponse.js"
 
 
-const createStudent = async (req,res)=>{
+const createStudent = async (req, res) => {
     // create a Student
-    const studentRes = await studentService.createStudent(req.body , req.user.classAssigned);
-    ApiResponse.created(res,"Student is created", studentRes)
+    const studentRes = await studentService.createStudent(req.body, req.user.classAssigned);
+    ApiResponse.created(res, "Student is created", studentRes)
 };
 
-const updateStudent = async (req,res)=>{
+const updateStudent = async (req, res) => {
     // register a user
-    const studentRes = await studentService.updateStudent(req.body,req.params.id, req.user.classAssigned);
-    ApiResponse.created(res,"Student updated successfully", studentRes)
+    const studentRes = await studentService.updateStudent(req.body, req.params.studentId, req.user.classAssigned);
+    ApiResponse.ok(res, "Student updated successfully", studentRes)
 };
 
-const getAllStudents = async (req,res)=>{
+const getAllStudents = async (req, res) => {
     // register a user
     const studentRes = await studentService.getAllStudents(req.user.classAssigned);
-    ApiResponse.created(res,"Fetched all students", studentRes)
+    ApiResponse.ok(res, "Fetched all students", studentRes)
 };
 
-const getStudentById = async (req,res)=>{
+const getStudentById = async (req, res) => {
     // register a user
-    const studentRes = await studentService.getStudentById(req.params.id , req.user.classAssigned);
-    ApiResponse.created(res,"Student fetched", studentRes)
+    const studentRes = await studentService.getStudentById(req.params.studentId, req.user.classAssigned);
+    ApiResponse.ok(res, "Student fetched", studentRes)
 };
-const deleteStudent = async (req,res)=>{
+const deleteStudent = async (req, res) => {
     // register a user
-    const studentRes = await studentService.deleteStudent(req.params.id , req.user.classAssigned);
-    ApiResponse.created(res,"Student deleted successfully", studentRes)
+    const studentRes = await studentService.deleteStudent(req.params.studentId, req.user.classAssigned);
+    ApiResponse.ok(res, "Student deleted successfully", studentRes)
 };
 
-export{
+export {
     createStudent,
     updateStudent,
     getAllStudents,
