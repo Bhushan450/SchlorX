4c7e45f171d2bc4fd73128b4ab05a6822bb93607
bhushan450 <bhushanmhaske45@gmail.com>
2026-09-15 22:16:37 +0530

    Refactor teacher routes for improved endpoint structure and consistency

--- backend/modules/teacher/teacher.routes.js
+++ backend/modules/teacher/teacher.routes.js
@@ -1,43 +1,41 @@
-import * as teacherController from "./admin.controller.js"
+import * as teacherController from "./teacher.controller.js"
 import { Router } from "express"
 import TeacherRegisterDto from "./dto/TeacherRegister.dto.js"
 import UpdateTeacherDto from "./dto/UpdateTeacher.dto.js"
 import UpdatePasswordDto from "./dto/updatePassword.dto.js"
 import validate from "../../common/middleware/validate.js"
 import validateObjectIds from "../../common/middleware/id_validator.js"
-import {authenticate, authorize} from "../auth/auth.middleware.js"
+import { authenticate, authorize } from "../auth/auth.middleware.js"
 
-const router = Router();    
+const router = Router();
 
 router.get(
-    '/teacher/:teacherId',
-    authenticate ,
+    '/:teacherId',
+    authenticate,
     authorize("admin"),
     validateObjectIds("teacherId"),
     teacherController.getTeacherById
 );
 
 router.patch(
-    '/teacher/:teacherId' ,
-    authenticate, 
+    '/updatePassword',
+    authenticate,
     authorize("teacher"),
-    validateObjectIds("teacherId"),
-    validate(UpdateTeacherDto), 
-    teacherController.updateTeacher 
+    validate(UpdatePasswordDto),
+    teacherController.changePassword
 );
 
 router.patch(
-    '/updatePassword' ,
-    authenticate, 
+    '/:teacherId',
+    authenticate,
     authorize("teacher"),
     validateObjectIds("teacherId"),
-    validate(UpdatePasswordDto), 
-    teacherController.changePassword 
+    validate(UpdateTeacherDto),
+    teacherController.updateTeacher
 );
 
-
 router.delete(
-    '/teacher/:teacherId' , 
+    '/:teacherId',
     authenticate,
     authorize("admin"),
     validateObjectIds("teacherId"),
