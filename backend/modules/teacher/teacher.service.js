71cc39f8846f2ba3052df38d1fbb5371d3d5f6e4
bhushan450 <bhushanmhaske45@gmail.com>
2026-09-15 22:16:46 +0530

    Refactor teacher service for improved error handling and code consistency

--- backend/modules/teacher/teacher.service.js
+++ backend/modules/teacher/teacher.service.js
@@ -3,39 +3,42 @@ import Class from "../class/class.model.js";
 import ApiError from "../../common/utils/ApiError.js";
 import { generateResetTokens } from "../../common/utils/jwt.js";
 
- //get teacher by id (admin)
- const getTeacherById = async (teacherId)=>{
+//get teacher by id (admin)
+const getTeacherById = async (teacherId) => {
 
-    if(!teacherId) throw ApiError.badRequest("Teacher id is required");
+    if (!teacherId) throw ApiError.badRequest("Teacher id is required");
 
     const teacher = await User.findOne({
-        _id:teacherId,
-        role:"teacher",
+        _id: teacherId,
+        role: "teacher",
     });
-    if(!teacher) throw ApiError.notFound("Teacher not found");
+    if (!teacher) throw ApiError.notFound("Teacher not found");
 
     return teacher;
- };
+};
 
- // update teacher (teacher)
- const updateTeacher = async (data,teacherId)=>{
+// update teacher (teacher)
+const updateTeacher = async (data, teacherId) => {
 
-    const {name,email,phone} = data;
+    if (!data) throw ApiError.badRequest("Data is required")
+    if (!teacherId) throw ApiError.badRequest("teacherID is required")
+
+    const { name, email, phone } = data;
 
     // find a user whose id:teacherId and role=="teacher"
     const teacher = await User.findOne({
-    _id: teacherId,
-    role: "teacher"
+        _id: teacherId,
+        role: "teacher"
     });
 
-    if(!teacher) throw ApiError.notFound("User not found");
+    if (!teacher) throw ApiError.notFound("User not found");
 
     // find user(admin/teacher) with this email whose id not equals to teacherId
     const existingEmail = await User.findOne({
         email,
-        _id:{ $ne:teacherId }
+        _id: { $ne: teacherId }
     });
-    if(existingEmail) throw ApiError.conflict("Email already exits! try different one")
+    if (existingEmail) throw ApiError.conflict("Email already exits! try different one")
 
     if (name) teacher.name = name;
     if (email) teacher.email = email;
@@ -44,17 +47,18 @@ import { generateResetTokens } from "../../common/utils/jwt.js";
     await teacher.save();
 
     return teacher;
- };
+};
 
- // change the user(teacher) password
- const changePassword = async (data, teacherId) => {
+// // change the user(teacher) password
+const changePassword = async (data, teacherId) => {
 
     const { currentPassword, newPassword } = data;
 
     if (!currentPassword || !newPassword) {
-        throw ApiError.badRequest("Current password and new password are required");}
+        throw ApiError.badRequest("Current password and new password are required");
+    }
 
-    const user = await User.findById(userId).select("+password");
+    const user = await User.findById(teacherId).select("+password");
 
     if (!user) throw ApiError.notFound("User not found");
 
@@ -71,34 +75,33 @@ import { generateResetTokens } from "../../common/utils/jwt.js";
     };
 };
 
- // delete teacher (teacher)
- const deleteTeacher = async (teacherId)=>{
-    if(!teacherId) throw ApiError.badRequest("teacherId required");
+// delete teacher (teacher)
+const deleteTeacher = async (teacherId) => {
+    if (!teacherId) throw ApiError.badRequest("teacherId required");
 
     const teacher = await User.findOne({
-        _id:teacherId,
-        role:"teacher"
+        _id: teacherId,
+        role: "teacher"
     });
-    if(!teacher) throw ApiError.notFound("Teacher not found");
+    if (!teacher) throw ApiError.notFound("Teacher not found");
 
     // find the class whose classTeacher is this 
-    const existingClass = await Class.findOne({classTeacher:teacherId});
-    if(existingClass)
-      {
-         existingClass.classTeacher = null; // remove the the teacher from its class
-         await existingClass.save();
-      } 
+    const existingClass = await Class.findOne({ classTeacher: teacherId });
+    if (existingClass) {
+        existingClass.classTeacher = null; // remove the the teacher from its class
+        await existingClass.save();
+    }
 
     // instead of deleting the user directly demote it from "teacher" to "user"
-      teacher.role = "user";
-      teacher.classAssigned = null;
-      await teacher.save();
-      
- };
+    teacher.role = "user";
+    teacher.classAssigned = null;
+    await teacher.save();
 
- export {
+};
+
+export {
     getTeacherById,
     updateTeacher,
     changePassword,
     deleteTeacher,
- }
+}
