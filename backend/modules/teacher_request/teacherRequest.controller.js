6279b8459f9908fa4a2bdea4bc72723f8547e823
bhushan450 <bhushanmhaske45@gmail.com>
2026-09-15 22:17:08 +0530

    Refactor teacher request controller for improved code consistency and readability

--- backend/modules/teacher_request/teacherRequest.controller.js
+++ backend/modules/teacher_request/teacherRequest.controller.js
@@ -1,39 +1,45 @@
 import ApiResponse from "../../common/utils/ApiResponse.js";
 import * as teacherRequestService from "./teacherRequest.service.js"
 
-const createTeacherRequest = async (req,res)=>{
+const createTeacherRequest = async (req, res) => {
     // create a request
     const requestRes = await teacherRequestService.createTeacherRequest(req.user.id);
-    ApiResponse.created(res,"TeacherRequest is created", requestRes)
-}; 
+    ApiResponse.created(res, "TeacherRequest is created", requestRes)
+};
 
-const getAllRequests = async (req,res)=>{
+const getAllRequests = async (req, res) => {
     // get allRequest
     const requestRes = await teacherRequestService.getAllRequests();
-    ApiResponse.created(res,"all TeacherRequests fetched", requestRes)
-}; 
+    ApiResponse.ok(res, "all TeacherRequests fetched", requestRes)
+};
 
-const getRequestById = async (req,res)=>{
+const getRequestById = async (req, res) => {
     // get RequestById
     const requestRes = await teacherRequestService.getRequestById(req.params.requestId);
-    ApiResponse.created(res,"teacherRequest fecthed", requestRes)
-}; 
+    ApiResponse.ok(res, "teacherRequest fecthed", requestRes)
+};
 
-const approveRequest = async (req,res)=>{
+const approveRequest = async (req, res) => {
     // approve request
     const requestRes = await teacherRequestService.approveRequest(req.params.requestId);
-    ApiResponse.created(res,"teacherRequest approved!", requestRes)
-}; 
-const rejectRequest = async (req,res)=>{
+    ApiResponse.ok(res, "teacherRequest approved!", requestRes)
+};
+const rejectRequest = async (req, res) => {
     // approve request
     const requestRes = await teacherRequestService.rejectRequest(req.params.requestId);
-    ApiResponse.created(res,"teacherRequest rejected!", requestRes)
-}; 
+    ApiResponse.ok(res, "teacherRequest rejected!", requestRes)
+};
+const getProcessedRequests = async (req, res) => {
 
-export{
+    const requests = await teacherRequestService.getProcessedRequests();
+    ApiResponse.ok(res, "Processed teacher requests fetched successfully", requests);
+};
+
+export {
     createTeacherRequest,
     getAllRequests,
     getRequestById,
     approveRequest,
     rejectRequest,
+    getProcessedRequests
 }
