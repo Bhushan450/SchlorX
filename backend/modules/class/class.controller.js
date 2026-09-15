ced3f97cc73df0c86d9aabae4df5f589f557b18a
bhushan450 <bhushanmhaske45@gmail.com>
2026-09-15 22:13:38 +0530

    Refactor class controller for consistent response handling and improved readability

--- backend/modules/class/class.controller.js
+++ backend/modules/class/class.controller.js
@@ -1,33 +1,33 @@
 import * as classService from "./class.service.js"
 import ApiResponse from "../../common/utils/ApiResponse.js"
 
-const createClass = async (req,res)=>{
+const createClass = async (req, res) => {
     // create a class
-    const classRes = await classService.createClass(req.body,req.user.id);
-    ApiResponse.created(res,"class is created", classRes)
+    const classRes = await classService.createClass(req.body, req.user.id);
+    ApiResponse.created(res, "class is created", classRes)
 };
 
-const updateClass = async (req,res)=>{
+const updateClass = async (req, res) => {
     // update a class
-    const classRes = await classService.updateClass(req.body , req.params.classId);
-    ApiResponse.created(res,"Updated class", classRes)
+    const classRes = await classService.updateClass(req.body, req.params.classId);
+    ApiResponse.ok(res, "Updated class", classRes)
 };
 
-const getAllclasses = async (req,res)=>{
+const getAllclasses = async (req, res) => {
     // get all classes
     const classRes = await classService.getAllclasses();
-    ApiResponse.created(res,"get all classes", classRes)
+    ApiResponse.ok(res, "get all classes", classRes)
 };
 
-const getClassById = async (req,res)=>{
+const getClassById = async (req, res) => {
     // get classById
     const classRes = await classService.getClassById(req.params.classId);
-    ApiResponse.created(res,"get class by id", classRes)
+    ApiResponse.ok(res, "get class by id", classRes)
 };
-const deleteClass = async (req,res)=>{
+const deleteClass = async (req, res) => {
     // delete a class
     const classRes = await classService.deleteClass(req.params.classId);
-    ApiResponse.created(res,"Class deleted", classRes)
+    ApiResponse.ok(res, "Class deleted", classRes)
 };
 
 export {
