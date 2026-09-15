563295c8d27f8e313390df70255c245e7764f497
bhushan450 <bhushanmhaske45@gmail.com>
2026-09-15 22:14:14 +0530

    Refactor UpdateClassDto for improved formatting and consistency

--- backend/modules/class/dto/updateClass.dto.js
+++ backend/modules/class/dto/updateClass.dto.js
@@ -1,13 +1,12 @@
 import BaseDto from "../../../common/dto/base.dto.js";
 import joi from "joi";
 
-class UpdateClassDto extends BaseDto{
-    
+class UpdateClassDto extends BaseDto {
+
     static schema = joi.object({
-        className : joi.string().trim().min(1).max(10).required(),
+        className: joi.string().trim().min(1).max(10).required(),
         section: joi.string().trim().default(null),
-        totalStudents : joi.number().min(0).default(0),
-        academicYear : joi.string().trim().min(4).max(10)
+        academicYear: joi.string().trim().min(4).max(10)
     })
 };
 
