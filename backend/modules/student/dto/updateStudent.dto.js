ebae1ef3ca8f5e0be32b4dddbc396819996c2fbb
bhushan450 <bhushanmhaske45@gmail.com>
2026-09-15 22:16:21 +0530

    Refactor UpdateStudentDto for improved code consistency and readability

--- backend/modules/student/dto/updateStudent.dto.js
+++ backend/modules/student/dto/updateStudent.dto.js
@@ -4,20 +4,20 @@ import BaseDto from "../../../common/dto/base.dto.js";
 class UpdateStudentDto extends BaseDto {
     static schema = Joi.object({
 
-    name: Joi.string().trim().min(1).max(20),
-    rollNo: Joi.number().integer().min(0).max(150),
-    DOB: Joi.date(),
-    gender: Joi.string().valid("male", "female", "other"),
-    parentPhone: Joi.string().trim().pattern(/^[0-9]{10}$/)
-    .messages({
-            "string.pattern.base": "Parent phone must be exactly 10 digits"
-        })
-})
+        name: Joi.string().trim().min(1).max(20),
+        rollNo: Joi.number().integer().min(0).max(150),
+        dob: Joi.date(),
+        gender: Joi.string().valid("Male", "Female", "Other"),
+        parentPhone: Joi.string().trim().pattern(/^[0-9]{10}$/)
+            .messages({
+                "string.pattern.base": "Parent phone must be exactly 10 digits"
+            })
+    })
 
-.min(1)
-.messages({
-    "object.min": "At least one field is required to update"
-});
+        .min(1)
+        .messages({
+            "object.min": "At least one field is required to update"
+        });
 }
 
 export default UpdateStudentDto;
