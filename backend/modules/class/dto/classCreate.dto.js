fa73e08e02a8b36c7a3b394113583b0ac9a0ad18
bhushan450 <bhushanmhaske45@gmail.com>
2026-09-15 22:14:04 +0530

    Refactor CreateClassDto for improved readability and schema consistency

--- backend/modules/class/dto/classCreate.dto.js
+++ backend/modules/class/dto/classCreate.dto.js
@@ -1,13 +1,12 @@
 import BaseDto from "../../../common/dto/base.dto.js";
 import joi from "joi";
 
-class CreateClassDto extends BaseDto{
-    
+class CreateClassDto extends BaseDto {
+
     static schema = joi.object({
-        className : joi.string().trim().min(1).max(10).required(),
-        classTeacher : joi.string().trim().default(null).required,
+        className: joi.string().trim().min(1).max(10).required(),
         section: joi.string().trim().default(null),
-        academicYear : joi.string().trim().min(4).max(10)
+        academicYear: joi.string().trim().min(4).max(10)
     })
 }
 
